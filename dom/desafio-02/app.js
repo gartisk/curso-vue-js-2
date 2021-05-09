b0264db7e935fc247ab91221086db4b40d0afecc
Guilherme Araujo <gartisk@gmail.com>
2021-05-08 22:17:41 -0300

    Resolução exercicio 2|25

--- dom/desafio-02/app.js
+++ dom/desafio-02/app.js
@@ -2,5 +2,13 @@ new Vue({
     el: '#desafio',
     data: {
         valor: ''
+    },
+    methods:{
+        clicouAqui: function(){
+            alert("Você clicou aqui!");
+        },
+        armazenarValor: function(event){
+            this.valor = event.target.value;
+        }
     }
 })
