5acaf7a831ae0e8d8680e1d8fa6f157e7aa72c62
Guilherme Araujo <gartisk@gmail.com>
2021-05-09 16:31:46 -0300

    minhas resposta desafio 3
     2|32. Hora de Praticar - Propriedades Reativas

---
+++ dom/desafio-03/app.js
@@ -0,0 +1,25 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        valor: 0,
+        contando: false
+    },
+    watch: {
+        valor() {
+            if( this.contando ){
+                return;
+            }
+            
+            this.contando = true;
+            setTimeout(() =>{
+                this.contando = false;
+                this.valor = 0;
+            }, 5000);
+        }
+    },
+    computed: {
+        resultado() {
+            return this.valor === 37 ? "Valor Igual a 37" : "Valor Diferente de 37";
+        }
+    }
+});
