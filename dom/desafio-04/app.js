6d32c9e4c4dad211a9cd29165762fd0be10faec2
Guilherme Araujo <gartisk@gmail.com>
2021-05-09 20:59:41 -0300

    Minha resolução desafio-4
    2|38. Hora de Praticar: Estilo

--- dom/desafio-04/app.js
+++ dom/desafio-04/app.js
@@ -1,14 +1,39 @@
 new Vue({
 	el: '#desafio',
 	data: {
-
+		efeito: '',
+		esperandoEfeito: false,
+		classeInputada: '',
+		classeInputada2: '',
+		fazerNovoInputDeClasse: false,
+		estiloDeclarado: '',
+		progresso: 0,
+		esperandoProgresso: 0
 	},
 	methods: {
 		iniciarEfeito() {
-
+			this.efeito = 'destaque';
 		},
 		iniciarProgresso() {
+			if( this.progresso > 0){
+				return;
+			}
 
+			setInterval( () => {
+				this.progresso = this.progresso < 100 ? this.progresso + 2 : 1;
+			}, 50);
+		}
+	},
+	watch: {
+		efeito() {
+			if( this.esperandoEfeito ){
+				return;
+			}
+			this.esperandoEfeito = true;
+
+			setInterval( () => {
+				this.efeito = this.efeito === 'encolher' ? 'destaque' : 'encolher';
+			}, 3000);
 		}
 	}
 })
