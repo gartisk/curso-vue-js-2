853aa6a501c2d72b660bf07e21eadd2f0134126b
Guilherme Araujo <gartisk@gmail.com>
2021-05-14 00:21:22 -0300

    4|65. Adicionando o Registro de Log #01

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -4,7 +4,8 @@
         data: {
             running: false,
             playerLife: 100,
-            monsterLife: 100
+            monsterLife: 100,
+            logs: []
         },
         computed: {
             hasResult() {
@@ -17,14 +18,16 @@
                 this.playerLife = 100;
                 this.monsterLife = 100;
             },
-            attack(special) {
-                this.hurt('monsterLife', 5, 10, special);
-                this.hurt('playerLife', 7, 12, false);
+            attack(isSpecial) {
+                this.hurt('monsterLife', 5, 10, isSpecial, 'Jogador', 'Monstro', 'player');
+                if( this.monsterLife < 1){ return; }
+                this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster');
             },
-            hurt(target, min, max, special) {
-                const plus = special ? 5 : 0;
+            hurt(target, min, max, isSpecial, from, to, cls) {
+                const plus = isSpecial ? 5 : 0;
                 const hurt = this.getRandom(min + plus, max + plus);
                 this[target] = Math.max(this[target] - hurt, 0);
+                this.registerLog(`${from} atingiu o ${to} com ${hurt}.`, cls);
             },
             healAndHurt(min, max) {
                 this.heal(10, 15);
@@ -37,6 +40,9 @@
             getRandom(min, max) {
                 const value = Math.random() * (max - min) + min;
                 return Math.round(value);
+            },
+            registerLog(text, cls) {
+                this.logs.unshift({ text, cls});
             }
 
         },
