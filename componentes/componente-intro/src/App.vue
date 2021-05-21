72ac8a7a87bde97a237c0acbf341d48c148f70ce
Guilherme Araujo <gartisk@gmail.com>
2021-05-21 00:59:05 -0300

    105. Usando Componentes

---
+++ componentes/componente-intro/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="app">
+    <app-contadores/>
+  </div>
+</template>
+<script>
+export default {
+ 
+}
+</script>
+<style>
+  
+</style>
---
+++ componentes/componente-intro/src/Contadores.vue
@@ -0,0 +1,18 @@
+<template lang="">
+    <div class="contadores">
+        <h1>Contadores</h1>
+        <app-contador v-for="c in 5" :key="c"/>
+    </div>
+</template>
+<script>
+import Contador from './Contador.vue'
+export default {
+    components: {
+        // inserido no escopo local
+        'app-contador': Contador
+    }
+}
+</script>
+<style>
+    
+</style>
