8494da8f1897e499c7c607e0a2b277e6cd318567
Guilherme Araujo <gartisk@gmail.com>
2021-05-21 01:53:01 -0300

    Minha Resposta Projeto Desafio Componente

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # curso-vue-js-2
 Udemy Curso Vue JS 2 
+
+### Util
+Para encerrar processo node indesejado:
+    
+    sudo lsof -nPi -sTCP:LISTEN
+
+    deverá listar:
+        ...
+        node      7087             nnn   22u  IPv4  88796      0t0  TCP *:8080 (LISTEN)
+
+    então:
+        pkill -9 7087
---
+++ componentes/componente-desafio/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <app-navigation></app-navigation>
+
+    <app-header></app-header>
+
+    <app-content></app-content>
+
+    <app-footer></app-footer>
+  </v-app>
+</template>
+
+<script>
+import Navigation from './components/Navigation.vue'
+import Header from './components/Header.vue'
+import Content from './components/Content.vue'
+import Footer from './components/Footer.vue'
+export default {
+  components: {
+      'app-navigation': Navigation,
+      'app-header': Header,
+      'app-content': Content,
+      'app-footer': Footer
+  }
+}
+</script>
